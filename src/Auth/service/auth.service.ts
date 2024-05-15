772383c5d2056f6ccf1dd060cba3eebe1986f010
shazaali <shaza.aly@gmail.com>
2024-05-15 14:21:11 +0300

    Add bcrypt for password hashing and compare in login method

--- src/Auth/service/auth.service.ts
+++ src/Auth/service/auth.service.ts
@@ -1,22 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../entity/user.auth';
 import { Repository } from 'typeorm';
 import { JwtService } from '@nestjs/jwt';
+import { LoginDto } from '../dto/login.dto';
+import { AuthResponse } from '../dto/auth-response.dto';
+import bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-    constructor(
-        private readonly jwtService: JwtService,
-        @InjectRepository(User)
-        private readonly userRepository : Repository<User>
-    ){}
+  constructor(
+    private readonly jwtService: JwtService,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
 
-    async login(loginDto: LoginDto): Promise<AuthResponse>{
-
+  async login(loginDto: LoginDto): Promise<AuthResponse> {
+    const user = await this.userRepository.findOne({
+        where: {email: loginDto.email}
+    })
+    if (!user) {
+        throw new UnauthorizedException('Invalid credentials');
+      }
+    const passwordMatch = await bcrypt.compare(loginDto.password, user.password)
+    if(user){
+        const token = this.jwtService.sign({id: user.id})
+        return {token, user}
     }
-
-
-    
-
+    return null
+  }
 }
